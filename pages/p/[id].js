0a1679864c7ba5a40df7905610a28dacefe1642e
Cijo KB <cijo.kb@gmail.com>
2020-04-02 10:13:57 +0530

    Data fetching in nextjs

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,27 @@
-import { useRouter } from 'next/router'
-import Layout from '../../components/Layout.js'
+import Layout from '../../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-  const router = useRouter()
-
+const Post = props => {
+  debugger;
   return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  )
-}
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium} /> : null}
+  </Layout>);
+};
+
+Post.getInitialProps = async function(context) {
+  debugger;
+  // this method will be executed in the client if the user navigated from the link
+  // this mehtod will be executed in the server if the user directly hit this link or refresh the  page
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
